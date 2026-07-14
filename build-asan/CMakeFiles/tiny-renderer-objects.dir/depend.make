fdd8b9f2ff2a82ed74edcceff5b186a2cc9ed452
WenchangGao <wenchang.gao@tufts.edu>
2026-07-14 23:57:54 +0800

    removed builds

--- build-asan/CMakeFiles/tiny-renderer-objects.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for tiny-renderer-objects.
-# This may be replaced when dependencies are built.
--- build-asan/CMakeFiles/tiny-renderer.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "tiny-renderer"
-  "tiny-renderer.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/tiny-renderer.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
